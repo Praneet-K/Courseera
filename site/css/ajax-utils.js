8d39819b24dfbc8c2cab9ea210de8ca64c9f30fe
Praneet-K <122003194@sastra.ac.in>
2020-08-20 12:15:57 +0530

    animie

---
+++ site/css/ajax-utils.js
@@ -0,0 +1,29 @@
+(function(global){
+	var ajaxUtils ={};
+
+	function getRequestObject() {
+		if (window.XMLHttpRequest) {
+			return (new XMLHttpRequest());
+		}
+		else {
+			global.alert("your browser doesnot support AJAX")
+			return(null);
+		}
+	}
+	ajaxUtils.sendGetRequest =
+	function (requestUrl, responseHandler){
+		var request=getRequestObject();
+		request.onreadystatechange =
+		function() {
+			handleResponse(request,responseHandler);
+		};
+		request.open("GET", requestUrl, true);
+		request.send(null);
+	};
+	function handleResponse(request,responseHandler){
+		if((request.readyState==4)&& (request.status ==200)){
+			responseHandler(request);
+		}
+	}
+	global.$ajaxUtils=ajaxUtils;
+})(window);
